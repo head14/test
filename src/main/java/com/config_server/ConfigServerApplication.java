c31250435426a7f22215b173b61edd8bbdf07a44
head14 <30287223+head14@users.noreply.github.com>
2019-09-01 13:38:55 +0800

    Update ConfigServerApplication.java

--- src/main/java/com/config_server/ConfigServerApplication.java
+++ src/main/java/com/config_server/ConfigServerApplication.java
@@ -11,7 +11,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		//123
+		//123456
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
